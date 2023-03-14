70a75f6cec29b522d4105dea6ee8aef6190da87d
hadesfranklyn <franklyn1998@live.com>
2023-03-14 11:55:36 -0300

    Returning Mocks and Testing the Book Service

---
+++ book-service/src/main/java/com/github/hadesfranklyn/project/model/Book.java
@@ -0,0 +1,159 @@
+package com.github.hadesfranklyn.project.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String author;
+	private Date launchDate;
+	private Double price;
+	private String title;
+	private String currency;
+	private String enviroment;
+
+	
+	// constructors
+	public Book() {
+	}
+
+	public Book(Long id, 
+			String author, 
+			String title, 
+			Date launchDate, 
+			Double price, 
+			String currency,
+			String enviroment) {
+		this.id = id;
+		this.author = author;
+		this.title = title;
+		this.launchDate = launchDate;
+		this.price = price;
+		this.currency = currency;
+		this.enviroment = enviroment;
+	}
+	
+	// get and set
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getLaunchDate() {
+		return launchDate;
+	}
+
+	public void setLaunchDate(Date launchDate) {
+		this.launchDate = launchDate;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public String getEnviroment() {
+		return enviroment;
+	}
+
+	public void setEnviroment(String enviroment) {
+		this.enviroment = enviroment;
+	}
+	
+	// hash and equals
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((currency == null) ? 0 : currency.hashCode());
+		result = prime * result + ((enviroment == null) ? 0 : enviroment.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((launchDate == null) ? 0 : launchDate.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (currency == null) {
+			if (other.currency != null)
+				return false;
+		} else if (!currency.equals(other.currency))
+			return false;
+		if (enviroment == null) {
+			if (other.enviroment != null)
+				return false;
+		} else if (!enviroment.equals(other.enviroment))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (launchDate == null) {
+			if (other.launchDate != null)
+				return false;
+		} else if (!launchDate.equals(other.launchDate))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	
+}
